32a5c6ebaa1fff7d5ed701994b51f8c403449142
Rudra <rudrajha202@gmail.com>
2025-02-07 01:12:19 +0530

    Add files via upload

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,92 @@
+import { SelectedPage, ClassType } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+import Class from "./Class";
+
+const classes: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Focuses on building muscle and improving overall strength through weightlifting, resistance exercises, and bodyweight routines.",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      " A challenging full-body workout designed to push you to your limits with a mix of cardio, strength, and endurance drills.",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Focuses on exercises that mimic daily activities, improving coordination, balance, and overall functional fitness.",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description:
+      " Classes like foam rolling, stretching, and mobility drills to aid recovery, prevent injuries, and maintain joint health.",
+    image: image6,
+  },
+];
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+            No matter your goal, our experienced instructors will guide you through every step to ensure you get the most out of each session. Join our classes to stay motivated, challenge yourself, and have fun while working toward your fitness goals!
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
